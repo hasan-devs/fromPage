2f0786e8cb883eb1008f64bcfca73fda87044552
hasan-devs <hasanm.dev@gmail.com>
2025-02-11 01:52:13 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ function App() {
 
   return (
     <div style={styles.container}>
-      <h1 style={styles.heading}>House Inquiry Form</h1>
+      <h1 style={styles.heading}>Hashoo Properties Limited - Investor/Buyer Query Form</h1>
       <form onSubmit={handleSubmit(onSubmit)} style={styles.form}>
         {/* Name */}
         <div style={styles.formGroup}>
